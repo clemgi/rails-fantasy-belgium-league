2aa5af08e2cddba6b04af2a862fd0bb879d7d31e
Larry Grutman <larry.grutman@gmail.com>
2016-08-23 11:04:21 +0200

    add routes, need to checked

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,12 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :squads, only: [:new]
+
+    resource :profile, only: [:show, :edit]
+
+  namespace :profile do
+    resources :squads, only: [:create, :edit, :show]
+    resources :leagues
+  end
 end
