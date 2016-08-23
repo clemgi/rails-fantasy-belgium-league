dcbbe263180ec4e6aa6761787496a06bd054bcd3
Larry Grutman <larry.grutman@gmail.com>
2016-08-23 23:15:22 +0200

    homepage first design + add logo navbar +add footer

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,5 +2,6 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [ :home ]
 
   def home
+    @background = ['old_trafford.jpg', 'stadium.jpg', 'stadium2.jpg'].sample
   end
 end
