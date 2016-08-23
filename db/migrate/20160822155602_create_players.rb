dab16e2c28c0650f95e571b12a018e086048a85d
Larry Grutman <larry.grutman@gmail.com>
2016-08-23 17:00:42 +0200

    update players DB field name

--- db/migrate/20160822155602_create_players.rb
+++ db/migrate/20160822155602_create_players.rb
@@ -1,8 +1,7 @@
 class CreatePlayers < ActiveRecord::Migration[5.0]
   def change
     create_table :players do |t|
-      t.string :first_name
-      t.string :last_name
+      t.string :name
       t.string :position
       t.integer :price
       t.references :team, foreign_key: true
